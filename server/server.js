d9b5829e95377e1bcce982bcc8fec2225179b396
Bohdan Lysov <BogUkraine@gmail.com>
2020-03-25 13:52:44 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const config = require('config');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoUri"), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+        app.listen(PORT, () => console.log(`Server is ready on port ${PORT}`))
+    }
+    catch (error) {
+        console.log('Server has not started :(', error);
+        process.exit(0);
+    }
+}
+
+start();
